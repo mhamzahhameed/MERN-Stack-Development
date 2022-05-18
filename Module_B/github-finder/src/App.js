84a7b2c89ccccf4a040f1ac09e7fe2dbd569ee73
Mhamzahhameed <hamzahraja11@gmail.com>
2022-05-18 11:23:06 +0500

    Added search functionality
    .1 Search.js created in the users component and linked to app.js
    .2 Alert.js is created in the layout component and linked with app component

--- Module_B/github-finder/src/App.js
+++ Module_B/github-finder/src/App.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter,} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
+import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
-  async componentDidMount() {
-    this.setState( { loading: true} );
-    const res = await axios.get('https://api.github.com/users') 
-    this.setState( { users: res.data, loading: false} );
+  // This function is called from the search component by passing props up
+  searchUsers = async (text) => {
+  this.setState( { loading: true} )
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`) 
+  this.setState( { users: res.data.items, loading: false} )
+  } 
+
+  // This function is called from the Search component to clear users from state
+  clearUsers = () => this.setState({users : [], loading: false})
+
+  // This fuction is called from the Search component to raise an alert for empty text field search
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type} })
+    // Remove the alert message after 5 seconds
+    setTimeout(() => this.setState({alert: null }), 5000)
   }
-  
+
   render () {
+    const { users, loading, alert} = this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={alert} />
+          <Search searchUsers={this.searchUsers} 
+                  clearUsers= {this.clearUsers}
+                  showClear={users.length > 0 ? true : false }
+                  setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
